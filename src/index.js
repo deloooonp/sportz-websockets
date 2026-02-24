8b833fa5e1a5c02c096eaea288e79da29a23affc
Delon Pratama <delonpratama1009@gmail.com>
2026-02-25 00:19:19 +0700

    implement matches POST and GET routes

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { matchRouter } from "./routes/matches.js";
+
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express Server!");
+});
+
+app.use(express.json());
+
+app.use("/matches", matchRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
